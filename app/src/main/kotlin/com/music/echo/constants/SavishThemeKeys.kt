db2c676cfae99cad1cb958f78efeb8ff25777a7c
vishallsinghh21 <vishal.v.s.singh21@gmail.com>
2026-09-05 21:55:14 +0530

    Add Savish Music Theme preference keys

---
+++ app/src/main/kotlin/com/music/echo/constants/SavishThemeKeys.kt
@@ -0,0 +1,12 @@
+package echo.music.iad1tya.constants
+
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+/** Preferences used by the Savish Music Theme layer. */
+val SavishMusicThemeEnabledKey = booleanPreferencesKey("savish_music_theme_enabled")
+val SavishPlatformKey = stringPreferencesKey("savish_platform")
+val SavishGlassHomeKey = booleanPreferencesKey("savish_glass_home")
+val SavishGlassSearchKey = booleanPreferencesKey("savish_glass_search")
+val SavishGlassSettingsKey = booleanPreferencesKey("savish_glass_settings")
+val SavishGlassContentKey = booleanPreferencesKey("savish_glass_content")
